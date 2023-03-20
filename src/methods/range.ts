84138dd863da3a6f6815795960cb471b16b6e8d0
g4rcez <allan.f.garcez@gmail.com>
2023-03-20 19:45:36 -0300

    wip

--- src/methods/range.ts
+++ src/methods/range.ts
@@ -1,7 +1,41 @@
-import { createChars, isNumberOrString } from "./utils";
+import { isNumber, isNumberOrString } from "./utils";
+
+const getInSequence = (a: string, b: string): [number, number] => {
+  let x = Number.parseInt(a, 10);
+  let y = Number.parseInt(b, 10);
+  return x > y ? [y, x] : [x, y];
+};
+
+const individualChars = (charA: string, charZ: string, jumps = 1) => {
+  const a = [];
+  let i = charA.charCodeAt(0);
+  const j = charZ.charCodeAt(0);
+  for (; i <= j; i += jumps) {
+    a.push(String.fromCharCode(i));
+  }
+  if (isNumber(charA) && isNumber(charZ)) {
+    return a.map((x) => Number.parseInt(x));
+  }
+  return a;
+};
+
+const createChars = (charA: string, charZ: string, jumps = 1) => {
+  let abs = Math.abs(jumps);
+  if (charA.length > 1 || charZ.length > 1) {
+    if (isNumber(charA) && isNumber(charZ)) {
+      const a = [];
+      const [first, second] = getInSequence(charA, charZ);
+      for (let i = first; i <= second; i += abs) {
+        a.push(i);
+      }
+      return a;
+    }
+  }
+  return individualChars(charA, charZ, abs);
+};
 
 export const range = <T extends number | string>(
-  firstOrLength: number | string,
+  firstOrLength: T,
   secondOrSteps?: number | string,
   jumps: number = 1
 ): T[] => {
--- src/methods/utils.ts
+++ src/methods/utils.ts
@@ -2,7 +2,6 @@ import { deepClone } from "./clone";
 import { ObjectMap } from "./typing";
 
 export const isObject = (a: any): a is Object => typeof a === "object";
-export const isFunction = (a: any): a is Function => typeof a === "function";
 
 export const equals = (a: any, b: any): boolean => {
   if (a === b) {
@@ -24,7 +23,7 @@ export const equals = (a: any, b: any): boolean => {
   return keys.every((k) => equals(a[k], b[k]));
 };
 
-const primitives = ["bigint", "boolean", "number", "string"];
+const primitives = ["bigint", "boolean", "number", "string", "undefined"];
 
 const isPrimitive = (e: unknown) => primitives.includes(typeof e);
 
@@ -49,40 +48,6 @@ export const sortBy = (key: string) => (a: ObjectMap, b: ObjectMap) => {
   return a[key] > b[key] ? 1 : -1;
 };
 
-const isNumber = (a: string) => /[0-9.]+/.test(a);
-
-const individualChars = (charA: string, charZ: string, jumps = 1) => {
-  const a = [];
-  let i = charA.charCodeAt(0);
-  const j = charZ.charCodeAt(0);
-  for (; i <= j; i += jumps) {
-    a.push(String.fromCharCode(i));
-  }
-  if (isNumber(charA) && isNumber(charZ)) {
-    return a.map((x) => Number.parseInt(x));
-  }
-  return a;
-};
-
-const getInSequence = (a: string, b: string): [number, number] => {
-  let x = Number.parseInt(a, 10);
-  let y = Number.parseInt(b, 10);
-  return x > y ? [y, x] : [x, y];
-};
-
-export const createChars = (charA: string, charZ: string, jumps = 1) => {
-  let abs = Math.abs(jumps);
-  if (charA.length > 1 || charZ.length > 1) {
-    if (isNumber(charA) && isNumber(charZ)) {
-      const a = [];
-      const [first, second] = getInSequence(charA, charZ);
-      for (let i = first; i <= second; i += abs) {
-        a.push(i);
-      }
-      return a;
-    }
-  }
-  return individualChars(charA, charZ, abs);
-};
+export const isNumber = (a: string) => /[0-9.]+/.test(a);
 
 export const isNumberOrString = (o: unknown): o is string | number => ["string", "number"].includes(typeof o);
